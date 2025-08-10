bdbb9f84116a16ea860a02fcf25b0ae9a3010c3d
MarcosVieira71 <marcosvieira120404@gmail.com>
2025-08-10 17:28:41 -0300

    mais testes

--- tests/models/user_test.go
+++ tests/models/user_test.go
@@ -109,3 +109,117 @@ func TestAddUserWithSameEmail(t *testing.T) {
 		t.Errorf("Usuário não deveria poder ser adicionado com email já cadastrado")
 	}
 }
+func TestDeleteUser(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	u, _ := user.CreateUser("Maria", "maria@email.com", "123")
+	_ = user.AddUser(db, u)
+
+	deleted, err := user.DeleteUser(db, int(u.Id))
+	if err != nil {
+		t.Fatalf("Erro ao deletar usuário: %v", err)
+	}
+
+	if deleted.Id != u.Id {
+		t.Errorf("Usuário deletado não corresponde ao esperado")
+	}
+
+	_, err = user.GetUserByID(db, int(u.Id))
+	if err == nil {
+		t.Errorf("Usuário deveria ter sido removido")
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	u, _ := user.CreateUser("Pedro", "Pedro@email.com", "123")
+	_ = user.AddUser(db, u)
+
+	updated, err := user.UpdateUser(db, int(u.Id), "Carlos Silva", "carlossilva@email.com", "456")
+	if err != nil {
+		t.Fatalf("Erro ao atualizar usuário: %v", err)
+	}
+
+	if updated.Name != "Carlos Silva" || updated.Email != "carlossilva@email.com" {
+		t.Errorf("Nome ou email não foram atualizados corretamente")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(updated.Password), []byte("456"))
+	if err != nil {
+		t.Errorf("Senha não foi atualizada corretamente")
+	}
+}
+
+func TestUpdateUserWithoutPassword(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	u, _ := user.CreateUser("Pedro", "Pedro@email.com", "123")
+	_ = user.AddUser(db, u)
+
+	updated, err := user.UpdateUser(db, int(u.Id), "Carlos Silva", "carlossilva@email.com", "")
+	if err != nil {
+		t.Fatalf("Erro ao atualizar usuário: %v", err)
+	}
+
+	if updated.Name != "Carlos Silva" || updated.Email != "carlossilva@email.com" {
+		t.Errorf("Nome ou email não foram atualizados corretamente")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(updated.Password), []byte("123"))
+	if err != nil {
+		t.Errorf("Senha foi atualizada incorretamente")
+	}
+}
+
+func TestGetAllUsers(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	u1, _ := user.CreateUser("Ana", "ana@email.com", "123")
+	u2, _ := user.CreateUser("Pedro", "pedro@email.com", "123")
+	_ = user.AddUser(db, u1)
+	_ = user.AddUser(db, u2)
+
+	usersList, err := user.GetAllUsers(db)
+	if err != nil {
+		t.Fatalf("Erro ao buscar todos os usuários: %v", err)
+	}
+
+	if len(usersList) != 2 {
+		t.Errorf("Esperado 2 usuários, mas obteve %d", len(usersList))
+	}
+}
+
+func TestDeleteUserNonExistent(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	_, err := user.DeleteUser(db, 999)
+	if err == nil {
+		t.Errorf("Deveria retornar erro ao tentar deletar usuário inexistente")
+	}
+}
+
+func TestUpdateUserNonExistent(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	_, err := user.UpdateUser(db, 999, "Novo Nome", "novo@email.com", "senha")
+	if err == nil {
+		t.Errorf("Deveria retornar erro ao tentar atualizar usuário inexistente")
+	}
+}
+
+func TestUpdateUserEmptyPassword(t *testing.T) {
+	db := tests.SetupTestDB(t)
+
+	u, _ := user.CreateUser("Lucas", "lucas@email.com", "123")
+	_ = user.AddUser(db, u)
+
+	updated, err := user.UpdateUser(db, int(u.Id), "Lucas Atualizado", "lucas@email.com", "")
+	if err != nil {
+		t.Fatalf("Erro ao atualizar usuário com senha vazia: %v", err)
+	}
+
+	if updated.Password != u.Password {
+		t.Errorf("Senha não deveria ser alterada quando a senha nova é vazia")
+	}
+}
